496f5d23a24325a64e7a98aea27b636e46815037
RealNumberI <102832908+RealNumberI@users.noreply.github.com>
2023-01-21 13:58:01 -0600

    Add files via upload
    Tweaked some comments.

--- process_streaming_TanyaFagaly.py
+++ process_streaming_TanyaFagaly.py
@@ -36,7 +36,7 @@ socket_type = socket.SOCK_DGRAM
 # use the socket constructor to create a socket object we'll call sock
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) 
 
-# read from a file to get some data
+# read from my file for the data I selected
 input_file = open("omophobia.csv", "r")
 
 # Declare a variable to hold the output file name
@@ -50,7 +50,7 @@ output_file = open(output_file_name, "w", newline='')
 # Create a csv writer for a comma delimited file
 writer = csv.writer(output_file, delimiter=",")
 
-# use the built0in sorted() function to get them in chronological order
+# use the built-in sorted() function to get them in chronological order
 reversed = sorted(input_file)
 
 # create a csv reader for our comma delimited data
@@ -65,7 +65,7 @@ for row in reader:
     COUNTRY,CSSSA_LEGAL,DATE_OF_DECRIM,MAX_PENALTY,CONST,BROAD_PROT,EMPLOY,HATE_CRIME,INCITEMENT,BAN_CONV_THERAPIES,SAME_SEX_MARRIAGE,CIVIL_UNIONS,JOINT_ADOPTION,SECOND_PARENT_ADOPTION = row
 
     # use an fstring to create a message from our data
-    # notice the f before the opening quote for our string?
+    # These are the data fields
     fstring_message = f"[{COUNTRY}, {CSSSA_LEGAL}, {DATE_OF_DECRIM}, {MAX_PENALTY}, {CONST}, {BROAD_PROT}, {EMPLOY}, {HATE_CRIME}, {INCITEMENT}, {BAN_CONV_THERAPIES}, {SAME_SEX_MARRIAGE}, {CIVIL_UNIONS}, {JOINT_ADOPTION}, {SECOND_PARENT_ADOPTION}]"
     
     writer.writerow(row)
@@ -77,7 +77,7 @@ for row in reader:
     sock.sendto(MESSAGE, address_tuple)
     print (f"Sent: {MESSAGE} on port {port}.")
 
-    # sleep for a few seconds
+    # sleep for a few seconds: try one second
     time.sleep(1)
 
 output_file.close()
